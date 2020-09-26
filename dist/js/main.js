acf58ae69b93289a97a0e432a05b6ad3273601f5
Milos Karaklajic <karaklajicmilos94@gmail.com>
2020-09-27 00:06:17 +0200

    first commit - finished up to the contact page

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+    scrollCue.init();
+    $(".owl-carousel").owlCarousel({
+
+        items : 1,
+        loop: true,
+        autoplay:true,
+        autoplaySpeed: 500,
+        dots: true,
+        // autoHeight: true,
+
+    });
+})(jQuery)
